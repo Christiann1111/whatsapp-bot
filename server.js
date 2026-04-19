14d0d359df45517ba9ef308079007bb3ab866178
Christian Casals Ortiz <155607906+Christiann1111@users.noreply.github.com>
2026-04-19 20:42:00 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+// Verificación de Meta
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "12345";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+// Recibir mensajes
+app.post("/webhook", (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Servidor corriendo"));
