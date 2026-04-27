23d86361bf61c70ba2aae7aaff4bcf3570015541
Christian Casals Ortiz <155607906+Christiann1111@users.noreply.github.com>
2026-04-27 15:08:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -57,6 +57,9 @@ async function getAIResponse(message) {
 
 // 📤 Enviar mensaje WhatsApp
 async function sendWhatsAppMessage(to, text) {
+  console.log("📤 ENTRANDO A WHATSAPP:", to, text);
+
+async function sendWhatsAppMessage(to, text) {
   try {
     const response = await fetch(`https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages`, {
       method: "POST",
